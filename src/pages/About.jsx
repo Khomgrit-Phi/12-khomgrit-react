68a1fd687276f835cfa2a940281f76665204fe7d
Khomgrit_Ton <khomgrit.ph@gmail.com>
2025-04-02 19:43:19 +0700

    feat(file): updated owner page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,16 @@ import React from "react";
 const About = () => {
   return (
     <div className="text-center p-4 bg-white rounded-md shadow-md">
-      <h1 className="text-2xl font-bold mb-2">About Us</h1>
-      <p className="text-gray-700">
-        This is a simple example app demonstrating React Router 7.4.0.
+      <h1 className="text-2xl font-bold mb-2">12-Khomgrit-Ton</h1>
+      <div className="flex w-3/6" >
+      <img src="./src/4.png" alt="profile image" className="mt-4"/>
+      <img src="./src/2.png" alt="profile image" className="mt-4"/>
+      
+      </div>
+      <p className="font-bold text-gray-700 mt-8">
+      "Self doubt and insecure".
       </p>
+      <p> visuals maker, music lovers, and a passionate learner.</p>
     </div>
   );
 };
