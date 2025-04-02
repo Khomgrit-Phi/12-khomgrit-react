6bbb5d2e8efb93b8d7b22c6cd9039d8063ecf9be
Khomgrit_Ton <khomgrit.ph@gmail.com>
2025-04-02 19:14:21 +0700

    feat(file): fix unloading page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import Products from "./pages/Owner";
+import Products from "./pages/About";
 import ProductDetail from "./pages/ProductDetail";
-import Owner from "./pages/Owner";
+import Owner from "./pages/About";
 
 const router = createBrowserRouter([
   {
